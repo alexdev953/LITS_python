be9933e697c5732628c227d31a16011db47d1ccb
alexdev953 <alexdev953@gmail.com>
2020-06-04 09:10:37 +0300

    Done

--- lesson 3/practic1.py
+++ lesson 3/practic1.py
@@ -4,17 +4,17 @@ while True:
     number = input('Enter number ')
     if number != 'stop':
         num = int(number)
-        if num < 0:
+        if num <= 0:
             list_1.append(num)
             continue
         elif num > 0:
             list_2.append(num)
             continue
-
     else:
         list_1.reverse()
         list_2.sort()
         list_2.extend(list_1)
-        print(list_2)
+        for values in list_2:
+            print(values, end=' ')
         break
 
